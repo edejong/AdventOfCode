42b004526bd86a0c64d4fc360043a5f01b928b8e
Erik de Jong <ecco256@gmail.com>
2021-12-27 22:24:34 +0100

    2021 Day 15 (work in progress)

--- 2021/Day15/Chiton.hs
+++ 2021/Day15/Chiton.hs
@@ -1,7 +1,8 @@
+{-# LANGUAGE TupleSections #-}
 module Day15.Chiton where
 
-
 -- TODO: Performance of this one is atrocious for Part 2
+-- https://github.com/bereal/AdventOfCodeHaskell/blob/main/src/Year2021/Day15.hs
 
 import Control.Monad.ST
 import Data.Array.ST
@@ -12,6 +13,7 @@ import qualified Data.Heap as H
 import Data.Heap (MinHeap)
 import Data.List (minimumBy, (\\), unfoldr)
 import Data.Function (on)
+import Data.Tuple (swap)
 
 type Point = (Int, Int)
 type Grid = Array Point Int
@@ -25,31 +27,35 @@ dijkstra grid = runSTArray $ do
     dist <- newArray bnds (maxBound::Int) :: ST s (STUArray s Point Int)
     writeArray dist (0,0) 0
 
-    let q = range bnds
-    -- q = H.fromList $ (0, (0, 0)) : (tail . map (maxBound::Int,) . range . bounds $ grid)
+    let q = H.fromList $ (0, (0, 0)) : (tail . map (maxBound::Int,) . range . bounds $ grid) :: MinQueue
     dijkstraIt grid dist prev q
 
     return prev
 
-dijkstraIt :: Grid -> STUArray s Point Int -> STArray s Point Point -> [Point] -> ST s ()
-dijkstraIt grid dist prev [] = return ()
+dijkstraIt :: Grid -> STUArray s Point Int -> STArray s Point Point -> MinQueue -> ST s ()
+dijkstraIt grid dist prev q | H.isEmpty q = return ()
 dijkstraIt grid dist prev q = do
     let bnds = bounds grid
 
-    u <- minElement dist q
-    let q' = q \\ [u]
+    let Just ((_,u), q') = H.view q
 
     dist_u <- readArray dist u
     -- let vs = filter (`elem` q') . neighbours8 bnds $ u
-    let vs = neighbours8 bnds $ u
+    let vs = neighbours8 bnds u
     let vs_alt = map ((+dist_u) . (grid !)) vs
     dist_vs <- readArr dist vs
     let tmp = filter (\(v,alt,dist_v) -> alt < dist_v) $ zip3 vs vs_alt dist_vs
 
-    writeArr dist $ map (\(v,alt,dist_v) -> (v,alt)) tmp
+    let dists = map (\(v,alt,dist_v) -> (v,alt)) tmp
+    writeArr dist $ dists
     writeArr' prev $ map (\(v,alt,dist_v) -> (v,u)) tmp
 
-    dijkstraIt grid dist prev q'
+    dijkstraIt grid dist prev (updatePrios (map swap dists) q')
+
+updatePrios :: [(Int, Point)] -> MinQueue -> MinQueue
+updatePrios prios = H.union q2 . H.filter (\(_, p') -> p' `notElem` tmp)
+  where tmp = map snd prios
+        q2 = H.fromList prios
 
 readArr :: (Ix i) => STUArray s i Int -> [i] -> ST s [Int]
 readArr arr [] = return []
@@ -97,8 +103,8 @@ main = do
     xs <- map (map digitToInt) . lines <$> readFile "2021/data/day15.txt"
     print $ solve xs
 
-    let xs' = [[((x-1)+k+p) `mod` 9 + 1| k <- [0..1], x <- line] | p <- [0..0], line <- xs]
-    print $ solve xs'
-
-    -- let xs' = [[((x-1)+k+p) `mod` 9 + 1| k <- [0..4], x <- line] | p <- [0..4], line <- xs]
+    -- let xs' = [[((x-1)+k+p) `mod` 9 + 1| k <- [0..1], x <- line] | p <- [0..0], line <- xs]
     -- print $ solve xs'
+
+    let xs' = [[((x-1)+k+p) `mod` 9 + 1| k <- [0..4], x <- line] | p <- [0..4], line <- xs]
+    print $ solve xs'
