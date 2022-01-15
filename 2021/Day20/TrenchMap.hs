5fb4a9bbb6ca10541f3b264b3e6a7307bc8b1766
Erik de Jong <ecco256@gmail.com>
2022-01-15 10:40:39 +0100

    Repa parallel improvements

--- 2021/Day20/TrenchMap.hs
+++ 2021/Day20/TrenchMap.hs
@@ -2,22 +2,25 @@
 {-# LANGUAGE ExplicitNamespaces #-}
 module Day20.TrenchMap where
 
-import qualified Data.Array.Repa as R
-import Data.Array.Repa (Source (Array, extent), DIM2, U, DIM1, D, Z (Z),type  (:.) ((:.)), sumAllS, fromListUnboxed, ix1, computeS, (!), inShape, ix2)
-import Data.List (unfoldr, foldl')
+import qualified Data.Array.Repa as R hiding (map)
+import Data.Array.Repa( (!), computeP, ix1, ix2, sumAllS, fromListUnboxed, inShape, Source(Array, extent), type (:.)((:.)), DIM1, DIM2, Z(Z), D, U )
 import Data.List.Split (chunksOf)
-import Text.Parsec ( endOfLine, oneOf, sepBy1, many )
-import Text.Parsec.String ( parseFromFile, Parser )
+import Data.List (foldl')
+import Text.Parsec.String (parseFromFile)
+import Text.Parsec (endOfLine, many, oneOf, sepBy1)
 
 type Image r = Array r DIM2 Int
 type EnhancementAlgo = Array U DIM1 Int
 
 main :: IO ()
 main = do
-    (algo, im) <- either (error . show) id <$> parseFromFile parser "2021/data/day20.txt"
-    let im' = enhanceN algo 50 im
-    putStrLn $ showImg im'
-    print $ sumAllS im'
+    (algo, im) <- either (error . show) id <$> parseFromFile parser "2021/data/day20-test.txt"
+    im1 <- enhanceN algo 2 im
+    im2 <- enhanceN algo 50 im
+    -- putStrLn $ showImg im1
+    print $ sumAllS im1
+    -- putStrLn $ showImg im2
+    print $ sumAllS im2
   where nop src = R.traverse src id id
 
 ------ Parser
@@ -36,12 +39,17 @@ showImg :: Image U -> String
 showImg im = unlines . chunksOf w . map toChar . R.toList $ im
   where (Z :. h :. w) = extent im
 
-enhanceN :: EnhancementAlgo -> Int -> Image U -> Image U
-enhanceN algo n src = fst . (!! n) . iterate (\(src', x:xs) -> (computeS $ enhance algo x src', xs)) $ (src, outOfBoundsValues)
+enhanceN :: (Monad m) => EnhancementAlgo -> Int -> Image U -> m (Image U)
+enhanceN algo n src = enhanceN' algo n src outOfBoundsValues
   where outOfBoundsValues
          | algo ! ix1 0 == 0 = repeat 0
          | algo ! ix1 511 == 0 = cycle [0, 1]
          | otherwise = 0 : repeat 1
+        enhanceN' _ 0 src _ = return src
+        enhanceN' algo n src (x:xs) = do
+            src' <- computeP $ enhance algo x src
+            enhanceN' algo (n-1) src' xs
+        enhanceN' _ _ _ _ = undefined
 
 enhance :: (Source r Int) => EnhancementAlgo -> Int -> Image r -> Image D
 enhance algo outOfBoundsValue src = R.traverse src resize draw
