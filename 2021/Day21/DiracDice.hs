e1a21f78eb708f2e1e206ac66992ce15062112b9
Erik de Jong <ecco256@gmail.com>
2022-01-21 14:50:59 +0100

    Changed DiracDice to bottom up dynamic programming. It's faster.

--- 2021/Day21/DiracDice.hs
+++ 2021/Day21/DiracDice.hs
@@ -1,18 +1,11 @@
 {-# LANGUAGE TypeApplications #-}
 module Day21.DiracDice where
-import Control.Monad.State
-    ( get, put, unless, runState, State, execState )
-import Prelude hiding (turn)
-import Data.Bifunctor (first, second, bimap)
-import Debug.Trace (trace)
-import Data.Char (isDigit)
 import Data.Tuple (swap)
-import Data.MemoTrie
-import Data.Array.Unboxed (array)
+import Data.Array.Unboxed (array, (!), Array)
 
 main :: IO ()
 main = do
-    xs <- map ((read @Integer) . drop 2 . dropWhile (/= ':')) . lines <$> readFile "2021/data/day21.txt"
+    xs <- map ((read @Integer) . drop 2 . dropWhile (/= ':')) . lines <$> readFile "2021/data/day21-test.txt"
     print $ uncurry (*) (playGame 1000 (head xs, 0) (xs !! 1, 0) 0)
     print $ uncurry max (countWins (head xs, 21) (xs !! 1, 21))
 
@@ -22,21 +15,22 @@ playGame maxScore p1@(pos1, score1) p2@(pos2, score2) numRolls =
     playGame maxScore p2 (pos1', score1') (numRolls + 3)
     where
         rolls = sum . map ((+1) . (`mod` 100) . (+numRolls)) $ [0, 1, 2]
-        pos1' = move pos1 rolls
+        pos1' = ((pos1 + rolls - 1) `mod` 10) + 1
         score1' = score1 + pos1'
 
-move :: Integer -> Integer -> Integer
-move pos n = ((pos + n - 1) `mod` 10) + 1
-
+-- Bottom up dynamic programming turns out to be about 1/3 faster than top down with
+-- memoization using MemoTrie
 countWins :: (Integer, Integer) -> (Integer, Integer) -> (Integer, Integer)
-countWins = memo2 countWins'
+countWins p1@(pos1, score1) p2@(pos2, score2) = cache ! (p1, p2)
   where
-    countWins' :: (Integer, Integer) -> (Integer, Integer) -> (Integer, Integer)
-    countWins' _ (_, score2) | score2 <= 0 = (0, 1)
-    countWins' p1@(pos1, score1) p2@(pos2, score2) =
-        swap $ foldr (\x memo -> memo `plus` countWins p2 (pos1' x, score1' x)) (0, 0) rolls
-        where
-            rolls = [d1+d2+d3 | d1 <- [1..3], d2 <- [1..3], d3 <- [1..3]]
-            pos1' = move pos1
-            score1' = (score1 -) . pos1'
+    cache :: Array ((Integer, Integer),(Integer, Integer)) (Integer, Integer)
+    cache = array (((1, 0), (1, 0)), ((10, 27), (10, 27))) 
+              [(((pos1, score1), (pos2, score2)), countWins' pos1 score1 pos2 score2) 
+              | score1 <- [0..27], score2 <- [0..27],  pos1 <- [1..10],  pos2 <- [1..10]]
+      where rolls = [d1+d2+d3 | d1 <- [1..3], d2 <- [1..3], d3 <- [1..3]]
             plus (x1, y1) (x2, y2) = (x1+x2, y1+y2)
+            countWins' pos1 score1 pos2 score2 = swap $ foldr (\x memo -> memo `plus` cached pos2 score2 (pos1' x) (score1' x)) (0, 0) rolls
+              where cached _ _ _ score2 | score2 <= 0 = (0, 1)
+                    cached pos1 score1 pos2 score2 = cache ! ((pos1, score1), (pos2, score2))
+                    pos1' n = ((pos1 + n - 1) `mod` 10) + 1
+                    score1' = (score1 -) . pos1'
