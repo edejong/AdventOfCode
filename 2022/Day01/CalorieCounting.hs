94df488a49d57fe739ae6515fe33bf3aac602e1f
Erik de Jong <ecco256@gmail.com>
2022-12-01 21:42:38 +0100

    Finished 2022 Day 1

--- 2022/Day01/CalorieCounting.hs
+++ 2022/Day01/CalorieCounting.hs
@@ -1,6 +1,11 @@
+{-# LANGUAGE TypeApplications #-}
+
 module Day01.CalorieCounting where
+
+import Data.List (sort)
+import Data.List.Split (splitOn)
 
 main :: IO ()
 main = do
-    xs <- readFile "2022/data/day01-test.txt"
-    print xs
+  xs <- reverse . sort . map (sum . map (read @Int) . words) . splitOn "\n\n" <$> readFile "2022/data/day01.txt"
+  print (sum . take 1 $ xs, sum . take 3 $ xs)
