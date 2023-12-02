c6c16c621498523d4efff4c2c73164a5b76a7a6e
Erik de Jong <ecco256@gmail.com>
2023-12-02 14:13:44 +0100

    Day 1

--- 2023/Day01/Trebuchet.hs
+++ 2023/Day01/Trebuchet.hs
@@ -1,6 +1,18 @@
+{-# LANGUAGE TypeApplications #-}
+
 module Day01.Trebuchet where
+
+import Control.Monad
+import Data.Char (isDigit)
+import Text.Parsec
+import Text.Parsec.String
 
 main :: IO ()
 main = do
-    xs <- readFile "2023/data/day01-test.txt"
-    print xs
+  xs <- lines <$> readFile "2023/data/day01.txt"
+  print $ sum . map ((\s -> read @Int [head s, last s]) . filter isDigit) $ xs
+  print $ sum <$> mapM (\s -> read @Int <$> sequence [parse firstNum "" s, parse lastNum "" s]) xs
+
+firstNum = digit <* many alphaNum <|> try (num <* many alphaNum) <|> alphaNum *> firstNum
+lastNum = try (alphaNum *> lastNum) <|> digit <* many alphaNum <|> num <* many alphaNum
+num = choice . zipWith (\i n -> i <$ string' n) ['1' ..] $ ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
