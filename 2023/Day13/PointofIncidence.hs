a1a7fa5529a4589ee59f74adad9ad9d722b8438c
Erik de Jong <ecco256@gmail.com>
2023-12-26 19:23:27 +0000

    Day 14

---
+++ 2023/Day13/PointofIncidence.hs
@@ -0,0 +1,15 @@
+import Data.List.Split (splitOn)
+import Data.List (tails, inits, transpose)
+import Data.Bifunctor (second, first)
+main :: IO ()
+main = do
+    xss <- map lines . splitOn "\n\n" <$> readFile "2023/Day13/day13.txt"
+    print (f 0 xss, f 1 xss)
+  where
+    f nSmudges = sum . map (\xs -> 100 * (sum . mirrorIndex nSmudges $ xs) + (sum . mirrorIndex nSmudges . transpose $ xs))
+
+mirrorIndex nSmudges = map fst . filter snd . zip [1..] . map ((== nSmudges) . uncurry mirrorDiff) . splits
+  where
+    splits xs = tail . init $ zip (inits xs) (tails xs)
+    mirrorDiff xs = sum . zipWith diff (reverse xs)
+    diff xs = length . filter id . zipWith (/=) xs
