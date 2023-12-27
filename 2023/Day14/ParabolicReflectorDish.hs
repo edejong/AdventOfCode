205b1aca650ea4e516416e9edbac43fede5183b0
Erik de Jong <ecco256@gmail.com>
2023-12-27 21:33:17 +0000

    Day 14

---
+++ 2023/Day14/ParabolicReflectorDish.hs
@@ -0,0 +1,32 @@
+import Data.List (sort, sortBy, transpose)
+import Data.Ord (Down (..), comparing)
+import FloydCycleDetect
+
+main :: IO ()
+main = do
+  xss <- lines <$> readFile "2023/Day14/day14.txt"
+  print $ weigh $ transpose . map tilt . transpose $ xss
+  let (mu, la) = findCycle cycleOnce xss
+  let k = (1000000000 - mu) `div` la
+  let x = fromInteger $ mu + (1000000000 - (mu + k * la))
+  print $ weigh $ iterate cycleOnce xss !! x
+
+tilt :: [Char] -> [Char]
+tilt ('#' : xs) = '#' : tilt xs
+tilt [] = []
+tilt xs =
+  let (as, bs) = span (/= '#') xs
+      k = length . filter (== 'O') $ as
+   in (replicate k 'O' ++ replicate (length as - k) '.') ++ tilt bs
+
+weigh :: [[Char]] -> Integer
+weigh = sum . map weigh' . transpose
+  where
+    weigh' = sum . map fst . filter ((== 'O') . snd) . zip [1 ..] . reverse
+
+cycleOnce :: [[Char]] -> [[Char]]
+cycleOnce xss =
+  let xssN = transpose . map tilt . transpose $ xss
+      xssW = map tilt xssN
+      xssS = reverse . transpose . map tilt . transpose . reverse $ xssW
+      xssE = map ((reverse . tilt) . reverse) xssS in xssE
---
+++ Algorithms/FloydCycleDetect.hs
@@ -0,0 +1,14 @@
+module FloydCycleDetect where
+
+{- Floyd Cycle Detection
+Returns (mu, la) where mu is the start of a cycle, la is the length of the cycle
+-}
+findCycle :: Eq a => (a -> a) -> a -> (Integer, Integer)
+findCycle f x0 =
+  let (_, x1) = run (1, 2) (move 1 x0) (move 2 x0) 0
+      (mu, x2) = run (1, 1) x0 x1 0
+      (la, _) = run (0, 1) x2 (move 1 x2) 1 in (mu,la)
+  where
+    move v x0 = iterate f x0 !! v
+    run (v1, v2) t h mu | t == h = (mu, h)
+                        | otherwise = run (v1, v2) (move v1 t) (move v2 h) (mu+1)
