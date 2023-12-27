1ca47b5603851800f8cf670f3c1b8c47e415c82b
Erik de Jong <ecco256@gmail.com>
2023-12-27 23:53:54 +0000

    Day 15

---
+++ 2023/Day15/LensLibrary.hs
@@ -0,0 +1,44 @@
+import Data.Char (isAlpha, ord)
+import Data.Foldable (toList)
+import Data.List (foldl')
+import Data.List.Split (splitOn)
+import Data.Map (Map)
+import Data.Sequence (Seq)
+import qualified Data.Sequence as S
+import Data.Word (Word8)
+
+-- import qualified Data.Map as M
+main :: IO ()
+main = do
+  xs <- splitOn "," <$> readFile "2023/Day15/day15.txt"
+  print $ sum $ map hash xs
+  let boxes = foldl' (flip doOp) (S.replicate 256 []) (map parseOp xs)
+  print $ sum . zipWith (\a -> (* a) . sum . zipWith (*) [1 ..] . map snd) [1 ..] . toList $ boxes
+
+hash :: String -> Int
+hash = fromIntegral . foldl' (\x -> (* 17) . (+ x)) (0 :: Word8) . map (fromIntegral . ord)
+
+type Lens = (String, Int)
+type Boxes = Seq [Lens]
+data Op = Remove String | Insert Lens deriving (Show)
+
+parseOp :: String -> Op
+parseOp xs =
+  let (as, bs) = span isAlpha xs
+   in case bs of
+        '=' : n -> Insert (as, read n)
+        "-" -> Remove as
+
+doOp :: Op -> Boxes -> Boxes
+doOp (Insert lens) = insertLens lens (hash . fst $ lens)
+doOp (Remove lbl) = removeLens lbl (hash lbl)
+
+insertLens :: Lens -> Int -> Boxes -> Boxes
+insertLens l@(lbl, fLen) = S.adjust insert
+  where
+    insert ls =
+      let (as, bs) = span ((/= lbl) . fst) ls
+       in as ++ (l : drop 1 bs)
+
+removeLens :: String -> Int -> Boxes -> Boxes
+removeLens lbl = S.adjust (filter ((/= lbl) . fst))
